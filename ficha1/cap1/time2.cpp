582eadad4e2f58d4fcce3f2fc6c43157d4845603
marineve17 <79589004+marineve17@users.noreply.github.com>
2021-03-01 16:54:35 +0000

    Add files via upload

---
+++ ficha1/cap1/time2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    int horas1, min1, seg1, horas2, min2, seg2, hfinal, mfinal, sfinal, days = 0, segtotal;
+    char c, s, x, y;
+    cout << "Input Time1 (hours:minutes:seconds): ";
+    cin >> horas1 >> c >> min1 >> s >> seg1;
+    cout << "Input Time2 (hours:minutes:seconds): ";
+    cin >> horas2 >> x >> min2 >> y >> seg2;
+
+    segtotal = horas1*3600 + horas2*3600 + min1*60 + min2*60 + seg1 + seg2;
+    days = segtotal/86400;
+    segtotal = segtotal%86400;
+    hfinal = segtotal/3600;
+    segtotal = segtotal % 3600;
+    mfinal = segtotal/60;
+    sfinal = segtotal % 60;
+
+    cout << "Time1 + Time2 = " << days << " day, " << hfinal << " hours, " << mfinal << " minutes and " << sfinal << " seconds.";
+    return -1;
+}
