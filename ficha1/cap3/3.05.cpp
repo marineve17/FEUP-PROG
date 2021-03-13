c87ac405e04a1cd493250ffbf3ad0c8737aeda23
marineve17 <mariana.lobao2002@gmail.com>
2021-03-13 19:03:29 +0000

    Fixed 3.5

--- ficha1/cap3/3.05.cpp
+++ ficha1/cap3/3.05.cpp
@@ -23,10 +23,32 @@ int getGCD(int a, int b) {
     return a;
 }
 
+int getGCD2(int& a, int& b) {
+    if (a == b) {
+        return a;
+    } else if ((a == 0) || (b == 0)) {
+        if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        }
+    } else {
+        while (!(a==b)) {
+            if (a>b) {
+                a -= b;
+            } else {
+                b -= a;
+            } 
+        }
+    }
+    return a;
+}
+
 int main() {
     int a, b;
     cout << "Input two numbers to get their greatest common divisor: ";
     cin >> a >> b;
-    cout << "The greatest common divisor (GCD) between both numers is " << getGCD(a, b) << endl;
+    cout << "The greatest common divisor (GCD) between both numbers is " << getGCD(a, b) << endl;
+    cout << "The greatest common divisor (GCD) between both numbers is (v2) " << getGCD2(a, b) << endl;
     return 0;
 }
